811a108f4b12dceb439be488d79b0ed5fc0134f0
aldifirmansyah <aldifirmansyh@gmail.com>
2019-09-11 12:49:17 +0700

    [ADD] models

---
+++ ARGOAGRO/ARGOAGRO/Models/ProductReviewModel.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARGOAGRO.Models
+{
+    public class ProductReviewModel
+    {
+        [PrimaryKey]
+        public int ID { get; set; }
+        public int ProductID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+    }
+}
