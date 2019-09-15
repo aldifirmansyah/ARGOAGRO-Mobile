d1e7f5e7de01644b936a7215f36b91483b38d7dc
aldifirmansyah <aldifirmansyh@gmail.com>
2019-09-15 10:16:41 +0700

    [ADD] Constructors in ProductReviewViewModel.cs

--- ARGOAGRO/ARGOAGRO/ViewModels/Presentation/ProductReviewViewModel.cs
+++ ARGOAGRO/ARGOAGRO/ViewModels/Presentation/ProductReviewViewModel.cs
@@ -1,16 +1,30 @@
-﻿using System;
+﻿using ARGOAGRO.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ARGOAGRO.ViewModels.Presentation
 {
-    class ProductReviewViewModel
+    public class ProductReviewViewModel
     {
-        public string ProductID { get; set; }
+        public int ProductID { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
         public string Location { get; set; }
         public string Description { get; set; }
         public int Rating { get; set; }
+
+        public ProductReviewViewModel() { }
+
+        public ProductReviewViewModel(ProductReviewModel obj)
+        {
+            ProductID = obj.ProductID;
+            FullName = obj.FullName;
+            Email = obj.Email;
+            Location = obj.Location;
+            Description = obj.Location;
+            Description = obj.Description;
+            Rating = obj.Rating;
+        }
     }
 }
