b1d1d344b6aa7d722d81c37b5cfa433d1a187fb7
aldifirmansyah <aldifirmansyh@gmail.com>
2019-09-24 11:58:03 +0700

    [ADD] Cari Produk progress

--- ARGOAGRO/ARGOAGRO/ViewModels/Logic/SearchProductPageViewModel.cs
+++ ARGOAGRO/ARGOAGRO/ViewModels/Logic/SearchProductPageViewModel.cs
@@ -3,11 +3,17 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xamarin.Forms;
 
 namespace ARGOAGRO.ViewModels
 {
 	public class SearchProductPageViewModel : BindableBase
 	{
+        public ImageSource MapImg
+        {
+            get { return ImageSource.FromResource("ARGOAGRO.Img.map.png"); }
+        }
+
         public SearchProductPageViewModel()
         {
 
