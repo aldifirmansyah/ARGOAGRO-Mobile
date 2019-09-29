be297018e178ce4509007618d830bad455738b94
rico.putra <rico.putra@outlook.co.id>
2019-09-29 13:32:55 +0700

    [UPDATE] Prepare for stars rating in ProdukDetailPage

--- ARGOAGRO/ARGOAGRO/ViewModels/Logic/ProdukDetailPageViewModel.cs
+++ ARGOAGRO/ARGOAGRO/ViewModels/Logic/ProdukDetailPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using ARGOAGRO.ViewModels.Presentation;
+﻿using ARGOAGRO.Services;
+using ARGOAGRO.ViewModels.Presentation;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
@@ -12,6 +13,7 @@ namespace ARGOAGRO.ViewModels
 	public class ProdukDetailPageViewModel : ViewModelBase
 	{
         private readonly INavigationService _navigationService;
+        private ProductService _productService = new ProductService();
 
         private ProductViewModel _product;
         public ProductViewModel Product
@@ -20,10 +22,11 @@ namespace ARGOAGRO.ViewModels
             set { SetProperty(ref _product, value); }
         }
 
-        public ImageSource BerasImg
-        {
-            get { return ImageSource.FromResource("ARGOAGRO.Img.beras.png"); }
-        }
+        public bool IsFirstOn { get; set; } = false;
+        public bool IsSecondOn { get; set; } = false;
+        public bool IsThirdOn { get; set; } = false;
+        public bool IsFourthOn { get; set; } = false;
+        public bool IsFifthOn { get; set; } = false;
 
         public ProdukDetailPageViewModel(INavigationService navigationService) : base(navigationService)
         {
@@ -32,11 +35,34 @@ namespace ARGOAGRO.ViewModels
 
         public override void OnNavigatedTo(INavigationParameters param)
         {
-            //var product = new ProductViewModel();
-
             if (!param.ContainsKey("productType")) return;
 
             Product = param.GetValue<ProductViewModel>("productType");
+            setStarsOn();
         }
-	}
+
+        /// <summary>
+        /// Set stars on for the rating of the product
+        /// </summary>
+        private void setStarsOn()
+        {
+            if (Product.Rating >= 1) IsFirstOn = true; else return;
+            if (Product.Rating >= 2) IsSecondOn = true; else return;
+            if (Product.Rating >= 3) IsThirdOn = true; else return;
+            if (Product.Rating >= 4) IsFourthOn = true; else return;
+            if (Product.Rating >= 5) IsFifthOn = true; else return;
+        }
+        
+
+        // Image Source
+
+        public ImageSource StarOnImg
+        {
+            get { return ImageSource.FromResource("ARGOAGRO.Img.star-48-on.png"); }
+        }
+        public ImageSource StarOffImg
+        {
+            get { return ImageSource.FromResource("ARGOAGRO.Img.star-48-off.png"); }
+        }
+    }
 }
