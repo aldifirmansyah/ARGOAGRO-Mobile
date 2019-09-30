a2ad4c138eb3f283c8b8751c9d519febad88bfd2
aldifirmansyah <aldifirmansyh@gmail.com>
2019-09-30 16:30:04 +0700

    [ADD] SQLiteDb.cs in ARGOAGRO.Droid (PREVIOUS ERROR REGARDING LOCAL DB)

---
+++ ARGOAGRO/ARGOAGRO.Android/Persistence/SQLiteDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using ARGOAGRO.Droid.Persistence;
+using ePMPro.Persistence;
+using SQLite;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+namespace ARGOAGRO.Droid.Persistence
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
