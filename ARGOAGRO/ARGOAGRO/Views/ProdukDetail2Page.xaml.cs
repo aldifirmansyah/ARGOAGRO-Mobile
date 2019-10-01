73fe741a40b50f454537958c21eaa2e4de8e2ea2
rico.putra <rico.putra@outlook.co.id>
2019-10-01 10:46:28 +0700

    [ADD] Prepare ProdukDetail2Page

---
+++ ARGOAGRO/ARGOAGRO/Views/ProdukDetail2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ARGOAGRO.Views
+{
+    public partial class ProdukDetail2Page : ContentPage
+    {
+        public ProdukDetail2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
