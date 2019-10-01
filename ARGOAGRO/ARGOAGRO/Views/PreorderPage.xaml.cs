713ab3b5e30dfd7df04c773e4395fb4a44221f74
rico.putra <rico.putra@outlook.co.id>
2019-10-01 14:21:07 +0700

    [ADD] Initiate PreorderPage

---
+++ ARGOAGRO/ARGOAGRO/Views/PreorderPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ARGOAGRO.Views
+{
+    public partial class PreorderPage : ContentPage
+    {
+        public PreorderPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
