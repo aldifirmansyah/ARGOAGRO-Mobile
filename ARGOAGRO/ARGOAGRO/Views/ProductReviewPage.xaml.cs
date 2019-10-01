718b08f0af752a5041bbde805af5ad15b5cea1f2
rico.putra <rico.putra@outlook.co.id>
2019-10-01 21:33:32 +0700

    [ADD] Initiate ProductReviewPage

---
+++ ARGOAGRO/ARGOAGRO/Views/ProductReviewPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ARGOAGRO.Views
+{
+    public partial class ProductReviewPage : ContentPage
+    {
+        public ProductReviewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
