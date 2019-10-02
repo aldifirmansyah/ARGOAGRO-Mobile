c32d7327188b5195a06e35dccb928c4bd24c0e51
rico.putra <rico.putra@outlook.co.id>
2019-10-02 13:55:17 +0700

    [ADD] Initiate ContactPage

---
+++ ARGOAGRO/ARGOAGRO/ViewModels/ContactPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace ARGOAGRO.ViewModels
+{
+	public class ContactPageViewModel : BindableBase
+	{
+        public ContactPageViewModel()
+        {
+
+        }
+
+        public ImageSource ContactImg
+        {
+            get { return ImageSource.FromResource("ARGOAGRO.Img.kontak_kami_header.jpg"); }
+        }
+
+        public ImageSource PhoneImg
+        {
+            get { return ImageSource.FromResource("ARGOAGRO.Img.call.png"); }
+        }
+
+    }
+}
