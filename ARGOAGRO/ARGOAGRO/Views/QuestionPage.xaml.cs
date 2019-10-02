df28aedcc02edd0680cb9f45cccc46ac5b3b5b0a
rico.putra <rico.putra@outlook.co.id>
2019-10-02 12:03:57 +0700

    [ADD] Initiate QuestionPage

---
+++ ARGOAGRO/ARGOAGRO/Views/QuestionPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ARGOAGRO.Views
+{
+    public partial class QuestionPage : ContentPage
+    {
+        public QuestionPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
