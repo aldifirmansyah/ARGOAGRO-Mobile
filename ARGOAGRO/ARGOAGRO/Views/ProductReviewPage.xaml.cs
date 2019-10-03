7cb08f62a69a0614dac93c0cda708a9e647fdd11
rico.putra <rico.putra@outlook.co.id>
2019-10-03 13:34:53 +0700

    [UPDATE] Prepare grid in code behind for review section

--- ARGOAGRO/ARGOAGRO/Views/ProductReviewPage.xaml.cs
+++ ARGOAGRO/ARGOAGRO/Views/ProductReviewPage.xaml.cs
@@ -1,12 +1,44 @@
-﻿using Xamarin.Forms;
+﻿using ARGOAGRO.ViewModels;
+using ARGOAGRO.ViewModels.Presentation;
+using System.Collections.Generic;
+using Xamarin.Forms;
 
 namespace ARGOAGRO.Views
 {
     public partial class ProductReviewPage : ContentPage
     {
+        private ProductReviewPageViewModel ProductReviewPageVM
+        {
+            get { return (ProductReviewPageViewModel) this.BindingContext; }
+        }
+
         public ProductReviewPage()
         {
             InitializeComponent();
+
+            IEnumerable<ProductReviewViewModel> reviews = ProductReviewPageVM.ProductReviews;
+            Grid gridReview;
+
+            foreach (ProductReviewViewModel review in reviews)
+            {
+                gridReview = new Grid();
+
+                // Grid Appearance
+                gridReview.RowSpacing = 0;
+                gridReview.VerticalOptions = LayoutOptions.EndAndExpand;
+                gridReview.Padding = new Thickness(0, 10, 0, 10);
+                gridReview.BackgroundColor = Color.FromHex("ffffff");
+
+                // Row and Column Definitions
+                gridReview.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                gridReview.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+                gridReview.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+                gridReview.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+                gridReview.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+
+                ReviewsStackLayout.Children.Add(gridReview);
+            }
+            
         }
     }
 }
