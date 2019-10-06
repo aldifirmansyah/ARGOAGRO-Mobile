b5d56fa854729f26449d512fd328e4d12a4674fc
rico.putra <rico.putra@outlook.co.id>
2019-10-06 10:14:00 +0700

    [UPDATE] add/sub button functionality in PreorderPage

--- ARGOAGRO/ARGOAGRO/ViewModels/Logic/PreorderPageViewModel.cs
+++ ARGOAGRO/ARGOAGRO/ViewModels/Logic/PreorderPageViewModel.cs
@@ -18,10 +18,24 @@ namespace ARGOAGRO.ViewModels
             get { return _product; }
             set { SetProperty(ref _product, value); }
         }
+        private int _orderQuantity;
+        public int OrderQuantity
+        {
+            get { return _orderQuantity; }
+            set { SetProperty(ref _orderQuantity, value); }
+        }
+       
+        // subtotal
 
         public DelegateCommand EnterShoppingCartCommand => new DelegateCommand(() =>
         {
             _navigationService.NavigateAsync("/MainPage/NavigationPage/HomePage");
+        });
+
+        public DelegateCommand<string> IsAddQuantityCommand => new DelegateCommand<string>((opr) =>
+        {
+            if (opr == "add") OrderQuantity++;
+            else if (OrderQuantity > 0) OrderQuantity--;
         });
 
         public PreorderPageViewModel(INavigationService navigationService) : base(navigationService)
